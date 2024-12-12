75b60093457339a18fd2029c2337dbad15944251
BonSeung <bs.koo@sqisoft.com>
2024-12-12 21:34:14 +0900

    initializing

---
+++ src/main/java/com/basic/basiccore/BasiccoreApplication.java
@@ -0,0 +1,13 @@
+package com.basic.basiccore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiccoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiccoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic/basiccore/BasiccoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.basiccore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiccoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
